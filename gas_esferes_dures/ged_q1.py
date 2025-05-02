59a15f4180fcc39280a09d9cc83bfc49a57ca39c
Arnau Vila <arnauvilacrespo@gmail.com>
2025-05-02 12:13:34 +0200

    tot be

--- gas_esferes_dures/ged_q1.py
+++ gas_esferes_dures/ged_q1.py
@@ -1,177 +1,207 @@
 from vpython import *
-#Web VPython 3.2
+import matplotlib.pyplot as plt
 
-# Hard-sphere gas.
+# Web VPython 3.2
 
-# Bruce Sherwood
+# Hard-sphere gas
 
 win = 500
-
-Natoms = 200  # change this to have more or fewer atoms
-
-# Typical values
-L = 1 # container is a cube L on a side
-gray = color.gray(0.7) # color of edges of container
-mass = 4E-3/6E23 # helium mass
-Ratom = 0.03 # wildly exaggerated size of helium atom
-k = 1.4E-23 # Boltzmann constant
-T = 300 # around room temperature
+Natoms = 500  # number of atoms
+L = 1  # container is a cube of side L
+gray = color.gray(0.7)
+mass = 4E-3/6E23  # helium mass
+k = 1.4E-23  # Boltzmann constant
+T = 300  # temperature
 dt = 1E-5
 
-animation = canvas( width=win, height=win, align='left')
+animation = canvas(width=win, height=win, align='left')
 animation.range = L
 animation.title = 'A "hard-sphere" gas'
 s = """  Theoretical and averaged speed distributions (meters/sec).
-  Initially all atoms have the same speed, but collisions
-  change the speeds of the colliding atoms. One of the atoms is
-  marked and leaves a trail so you can follow its path.
-  
+Initially all atoms have the same speed, but collisions
+change the speeds of the colliding atoms. One of the atoms is
+marked and leaves a trail so you can follow its path.
 """
 animation.caption = s
 
-d = L/2+Ratom
 r = 0.005
-boxbottom = curve(color=gray, radius=r)
-boxbottom.append([vector(-d,-d,-d), vector(-d,-d,d), vector(d,-d,d), vector(d,-d,-d), vector(-d,-d,-d)])
-boxtop = curve(color=gray, radius=r)
-boxtop.append([vector(-d,d,-d), vector(-d,d,d), vector(d,d,d), vector(d,d,-d), vector(-d,d,-d)])
-vert1 = curve(color=gray, radius=r)
-vert2 = curve(color=gray, radius=r)
-vert3 = curve(color=gray, radius=r)
-vert4 = curve(color=gray, radius=r)
-vert1.append([vector(-d,-d,-d), vector(-d,d,-d)])
-vert2.append([vector(-d,-d,d), vector(-d,d,d)])
-vert3.append([vector(d,-d,d), vector(d,d,d)])
-vert4.append([vector(d,-d,-d), vector(d,d,-d)])
+deltav = 100
 
-Atoms = []
-p = []
-apos = []
-pavg = sqrt(2*mass*1.5*k*T) # average kinetic energy p**2/(2mass) = (3/2)kT
+gg = graph(width=win, height=0.4*win, xmax=3000, align='left',
+           xtitle='speed, m/s', ytitle='Number of atoms', ymax=Natoms*deltav/1000)
+
+
+# Llista de valors del radi dels àtoms
+N=40
+Radi_inicial=0.01
+Radis=[0]*N
+
+for i in range(N):
+  Radis[i]=Radi_inicial+i*(0.1-Radi_inicial)/(N-1)
     
-for i in range(Natoms):
-    x = L*random()-L/2
-    y = L*random()-L/2
-    z = L*random()-L/2
-    if i == 0:
-        Atoms.append(sphere(pos=vector(x,y,z), radius=Ratom, color=color.cyan, make_trail=True, retain=100, trail_radius=0.3*Ratom))
-    else: Atoms.append(sphere(pos=vector(x,y,z), radius=Ratom, color=gray))
-    apos.append(vec(x,y,z))
-    theta = pi*random()
-    phi = 2*pi*random()
-    px = pavg*sin(theta)*cos(phi)
-    py = pavg*sin(theta)*sin(phi)
-    pz = pavg*cos(theta)
-    p.append(vector(px,py,pz))
+temps_mitja = []  #Llista de valors del temps mitjà entre col·lisions per a cada valor del radi dels àtoms
+temps_mitja_teoric = []  #Llista de valors del temps mitjà teòric entre col·lisions per a cada valor del radi dels àtoms
 
-deltav = 100 # binning for v histogram
+pavg = sqrt(2 * mass * 1.5 * k * T)
 
-def barx(v):
-    return int(v/deltav) # index into bars array
+# Càlcul dels valors del temps mitjà teòric en funció del radi dels àtoms
+for i in Radis:
+    d = 2*i  #Càlcul de la secció eficaç
+    densitat_particules = Natoms / (L ** 3)  #Càlcul de la densitat de partícules
+    velocitat_mitjana = pavg / mass    #Càlcul de la velocitat mitjana
+    t_mitja_teoric= 2 / (sqrt(2) * pi * (densitat_particules ** 2) * (d**2) * velocitat_mitjana)
+    temps_mitja_teoric.append(t_mitja_teoric)
 
-nhisto = int(4500/deltav)
-histo = []
-for i in range(nhisto): histo.append(0.0)
-histo[barx(pavg/mass)] = Natoms
-
-gg = graph( width=win, height=0.4*win, xmax=3000, align='left',
-    xtitle='speed, m/s', ytitle='Number of atoms', ymax=Natoms*deltav/1000)
-
-theory = gcurve( color=color.blue, width=2 )
-dv = 10
-for v in range(0,3001+dv,dv):  # theoretical prediction
-    theory.plot( v, (deltav/dv)*Natoms*4*pi*((mass/(2*pi*k*T))**1.5) *exp(-0.5*mass*(v**2)/(k*T))*(v**2)*dv )
-
-accum = []
-for i in range(int(3000/deltav)): accum.append([deltav*(i+.5),0])
-vdist = gvbars(color=color.red, delta=deltav )
-
-def interchange(v1, v2):  # remove from v1 bar, add to v2 bar
-    barx1 = barx(v1)
-    barx2 = barx(v2)
-    if barx1 == barx2:  return
-    if barx1 >= len(histo) or barx2 >= len(histo): return
-    histo[barx1] -= 1
-    histo[barx2] += 1
+for Ratom in Radis:
     
-def checkCollisions():
-    hitlist = []
-    r2 = 2*Ratom
-    r2 *= r2
+    d = L / 2 + Ratom
+    boxbottom = curve(color=gray, radius=r)
+    boxbottom.append([vector(-d, -d, -d), vector(-d, -d, d), vector(d, -d, d), vector(d, -d, -d), vector(-d, -d, -d)])
+    boxtop = curve(color=gray, radius=r)
+    boxtop.append([vector(-d, d, -d), vector(-d, d, d), vector(d, d, d), vector(d, d, -d), vector(-d, d, -d)])
+    vert1 = curve(color=gray, radius=r)
+    vert2 = curve(color=gray, radius=r)
+    vert3 = curve(color=gray, radius=r)
+    vert4 = curve(color=gray, radius=r)
+    vert1.append([vector(-d, -d, -d), vector(-d, d, -d)])
+    vert2.append([vector(-d, -d, d), vector(-d, d, d)])
+    vert3.append([vector(d, -d, d), vector(d, d, d)])
+    vert4.append([vector(d, -d, -d), vector(d, d, -d)])
+
+    Atoms = []
+    p = []
+    apos = []
+
     for i in range(Natoms):
-        ai = apos[i]
-        for j in range(i) :
-            aj = apos[j]
-            dr = ai - aj
-            if mag2(dr) < r2: hitlist.append([i,j])
-    return hitlist
+        x = L * random() - L / 2
+        y = L * random() - L / 2
+        z = L * random() - L / 2
+        if i == 0:
+            Atoms.append(sphere(pos=vector(x, y, z), radius=Ratom, color=color.cyan,
+                                make_trail=True, retain=100, trail_radius=0.3 * Ratom))
+        else:
+            Atoms.append(sphere(pos=vector(x, y, z), radius=Ratom, color=gray))
+        apos.append(vec(x, y, z))
+        theta = pi * random()
+        phi = 2 * pi * random()
+        px = pavg * sin(theta) * cos(phi)
+        py = pavg * sin(theta) * sin(phi)
+        pz = pavg * cos(theta)
+        p.append(vector(px, py, pz))
 
-nhisto = 0 # number of histogram snapshots to average
+    nhisto = int(4500 / deltav)
+    histo = [0.0] * nhisto
+    histo[int(pavg / mass / deltav)] = Natoms
 
-while True:
-    rate(300)
-    # Accumulate and average histogram snapshots
-    for i in range(len(accum)): accum[i][1] = (nhisto*accum[i][1] + histo[i])/(nhisto+1)
-    if nhisto % 10 == 0:
-        vdist.data = accum
-    nhisto += 1
+    accum = [[deltav * (i + 0.5), 0] for i in range(int(3000 / deltav))]
+    vdist = gvbars(color=color.red, delta=deltav)
 
-    # Update all positions
-    for i in range(Natoms): Atoms[i].pos = apos[i] = apos[i] + (p[i]/mass)*dt
-    
-    # Check for collisions
-    hitlist = checkCollisions()
+    def barx(v):
+        return int(v / deltav)
 
-    # If any collisions took place, update momenta of the two atoms
-    for ij in hitlist:
-        i = ij[0]
-        j = ij[1]
-        ptot = p[i]+p[j]
-        posi = apos[i]
-        posj = apos[j]
-        vi = p[i]/mass
-        vj = p[j]/mass
-        vrel = vj-vi
-        a = vrel.mag2
-        if a == 0: continue;  # exactly same velocities
-        rrel = posi-posj
-        if rrel.mag > Ratom: continue # one atom went all the way through another
+    def interchange(v1, v2):
+        barx1 = barx(v1)
+        barx2 = barx(v2)
+        if barx1 == barx2:
+            return
+        if barx1 >= len(histo) or barx2 >= len(histo):
+            return
+        histo[barx1] -= 1
+        histo[barx2] += 1
+
+    def checkCollisions():
+        hitlist = []
+        r2 = 2 * Ratom
+        r2 *= r2
+        for i in range(Natoms):
+            ai = apos[i]
+            for j in range(i):
+                aj = apos[j]
+                dr = ai - aj
+                if mag2(dr) < r2:
+                    hitlist.append([i, j])
+        return hitlist
+
+    temps_total_simulacio = 0  # Afegim un contador del temps total de simulació
+    num_colisions = 0      # Afegim un contador del numero total de colisions
     
-        # theta is the angle between vrel and rrel:
-        dx = dot(rrel, vrel.hat)       # rrel.mag*cos(theta)
-        dy = cross(rrel, vrel.hat).mag # rrel.mag*sin(theta)
-        # alpha is the angle of the triangle composed of rrel, path of atom j, and a line
-        #   from the center of atom i to the center of atom j where atome j hits atom i:
-        alpha = asin(dy/(2*Ratom)) 
-        d = (2*Ratom)*cos(alpha)-dx # distance traveled into the atom from first contact
-        deltat = d/vrel.mag         # time spent moving from first contact to position inside atom
-        
-        posi = posi-vi*deltat # back up to contact configuration
-        posj = posj-vj*deltat
-        mtot = 2*mass
-        pcmi = p[i]-ptot*mass/mtot # transform momenta to cm frame
-        pcmj = p[j]-ptot*mass/mtot
-        rrel = norm(rrel)
-        pcmi = pcmi-2*pcmi.dot(rrel)*rrel # bounce in cm frame
-        pcmj = pcmj-2*pcmj.dot(rrel)*rrel
-        p[i] = pcmi+ptot*mass/mtot # transform momenta back to lab frame
-        p[j] = pcmj+ptot*mass/mtot
-        apos[i] = posi+(p[i]/mass)*deltat # move forward deltat in time
-        apos[j] = posj+(p[j]/mass)*deltat
-        interchange(vi.mag, p[i].mag/mass)
-        interchange(vj.mag, p[j].mag/mass)
-    
-    for i in range(Natoms):
-        loc = apos[i]
-        if abs(loc.x) > L/2:
-            if loc.x < 0: p[i].x =  abs(p[i].x)
-            else: p[i].x =  -abs(p[i].x)
-        
-        if abs(loc.y) > L/2:
-            if loc.y < 0: p[i].y = abs(p[i].y)
-            else: p[i].y =  -abs(p[i].y)
-        
-        if abs(loc.z) > L/2:
-            if loc.z < 0: p[i].z =  abs(p[i].z)
-            else: p[i].z =  -abs(p[i].z)
-    
+    nhisto = 0
+
+    while True:
+        rate(300)
+        temps_total_simulacio += dt
+
+        for i in range(len(accum)):
+            accum[i][1] = (nhisto * accum[i][1] + histo[i]) / (nhisto + 1)
+        if nhisto % 10 == 0:
+            vdist.data = accum
+        nhisto += 1
+
+        for i in range(Natoms):
+            Atoms[i].pos = apos[i] = apos[i] + (p[i] / mass) * dt
+
+        hitlist = checkCollisions()
+
+        for ij in hitlist:
+            i, j = ij
+            ptot = p[i] + p[j]
+            posi = apos[i]
+            posj = apos[j]
+            vi = p[i] / mass
+            vj = p[j] / mass
+            vrel = vj - vi
+            if vrel.mag2 == 0:
+                continue
+            rrel = posi - posj
+            if rrel.mag > Ratom:
+                continue
+
+            dx = dot(rrel, vrel.hat)
+            dy = cross(rrel, vrel.hat).mag
+            alpha = asin(dy / (2 * Ratom))
+            d = (2 * Ratom) * cos(alpha) - dx
+            deltat = d / vrel.mag
+
+            posi -= vi * deltat
+            posj -= vj * deltat
+            mtot = 2 * mass
+            pcmi = p[i] - ptot * mass / mtot
+            pcmj = p[j] - ptot * mass / mtot
+            rrel = norm(rrel)
+            pcmi -= 2 * pcmi.dot(rrel) * rrel
+            pcmj -= 2 * pcmj.dot(rrel) * rrel
+            p[i] = pcmi + ptot * mass / mtot
+            p[j] = pcmj + ptot * mass / mtot
+            apos[i] = posi + (p[i] / mass) * deltat
+            apos[j] = posj + (p[j] / mass) * deltat
+            interchange(vi.mag, p[i].mag / mass)
+            interchange(vj.mag, p[j].mag / mass)
+
+            num_colisions += 1
+
+        for i in range(Natoms):
+            loc = apos[i]
+            if abs(loc.x) > L / 2:
+                p[i].x *= -1
+            if abs(loc.y) > L / 2:
+                p[i].y *= -1
+            if abs(loc.z) > L / 2:
+                p[i].z *= -1
+
+        if temps_total_simulacio >= 0.001001: #Aquest temps és el que tarda en estabilitzar-se considerablement el sistema.
+            break
+
+    temps_mitja.append(temps_total_simulacio / num_colisions)
+
+# Gràfic tiemps mitjà vs radi de l'àtom
+
+
+plt.style.use('classic')
+plt.figure(facecolor='white')
+plt.plot(Radis, temps_mitja, label='Simulació', color='blue')
+plt.plot(Radis, temps_mitja_teoric, label='Teòric', color='red')
+plt.legend()
+plt.xlabel("Radi de l'àtom (m)")
+plt.ylabel("Tiemps mitjà entre col·lisions (s)")
+plt.grid(True)
+plt.show()
